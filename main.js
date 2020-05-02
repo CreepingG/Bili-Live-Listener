36bcef183b3a5375690616cbbeaff9c45069c148
东风有意 <791141513@qq.com>
2020-05-02 14:29:31 +0800

    new

--- main.js
+++ main.js
@@ -1,41 +1,108 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow, Tray, Menu} = require('electron');
+const path = require('path');
+const notifier = require('node-notifier');
+
+const nodeConsole = require('console');
+const console = new nodeConsole.Console(process.stdout, process.stderr);
+
+// 使用浏览器打开网页
+function Open(url){
+	let c = require('child_process');
+	c.exec('start ' + url);
+}
 
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+	// Create the browser window.
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true // 允许require
+		}
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+	// and load the index.html of the app.
+	win.loadFile('index.html');
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+	// 关闭上方菜单
+	win.setMenu(null);
+	
+	// 覆写最小化行为
+	win.on('minimize', (event) => {
+		win.hide();
+		win.setSkipTaskbar(true);
+		event.preventDefault();
+	});
+
+	// 创建系统通知区菜单
+	const tray = new Tray(path.join(__dirname, 'icon.ico'));
+	tray.setToolTip('Bili开播提醒');
+	tray.setContextMenu(Menu.buildFromTemplate([
+		{label: '退出', click: () => {win.destroy();}},// 真正的退出
+	]));
+	tray.on('click', ()=>{ // 单击托盘图标时切换应用显隐
+		if (win.isVisible()){
+			win.hide();
+			win.setSkipTaskbar(true); // 下方任务栏按钮
+		}
+		else{
+			win.show();
+			win.setSkipTaskbar(false);
+		}
+	});
+
+	// 进程间通信
+	const ipcMain = require('electron').ipcMain;
+	ipcMain.on('asynchronous-message', function(event, arg) {
+		console.log(arg);
+		arg = JSON.parse(arg);
+		let action = arg.action;
+		if (action === 'send'){
+			let {title, body, url} = arg;
+			// event.sender.send('asynchronous-reply', 'pong');
+			// let notification = new Notification(title, {body});
+			// notification.show();
+			notifier.notify({
+				title: title,
+				message: body,
+				sound: true,
+				appID: '开播提醒'
+			}, (error, type, info)=>{
+				if (type !== 'timeout'){
+					Open(url);
+				}
+			});
+		}
+		else if (action === 'open'){
+			Open(arg.url);
+		}
+		else if(action === 'f12'){
+			win.webContents.openDevTools();
+		}
+	});
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') app.quit();
+});
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.setAppUserModelId("com.electron.bili-live-reminder");
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,6 +1,94 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+/* eslint-disable no-console */
+const {ipcRenderer, axios} = window;
+
+ipcRenderer.on('asynchronous-reply', function(event, arg) {
+    console.log(arg);
+});
+
+const btn = document.getElementById('btn');
+const input = document.getElementById('input');
+
+function GetRawInfo(roomid){
+    return axios.get('https://api.live.bilibili.com/xlive/web-room/v1/index/getInfoByRoom?room_id=' + roomid).then(result=>result.data.data);
+}
+async function GetParsedInfo(roomid){
+    let info = await GetRawInfo(roomid);
+    console.log(info);
+    let room = info.room_info;
+    let user = info.anchor_info.base_info;
+    return {
+        roomid: roomid,
+        status: room.live_status,
+        title: room.title,
+        cover: room.cover,
+        url: 'https://live.bilibili.com/' + roomid,
+        name: user.uname,
+        avatar: user.face,
+    };
+}
+
+function ShowList(list){
+    let ul = document.getElementById('list');
+    ul.innerText = '';
+    list.forEach(info=>{
+        let li = document.createElement('li');
+        li.innerHTML = info.name + '：' + (info.status ? info.title : '<span style="color:gray">未开播</span>');
+        if (info.status){
+            li.style.cursor = 'pointer';
+            li.onclick = function(){
+                ipcRenderer.send('asynchronous-message', JSON.stringify({
+                    action: 'open',
+                    url: info.url
+                }));
+            };
+        }
+        ul.appendChild(li);
+    });
+}
+function SendNotification(title, body, url){
+    ipcRenderer.send('asynchronous-message', JSON.stringify({
+        action: 'send',
+        title,
+        body,
+        url
+    }));
+}
+
+let cnt = 0;
+
+btn.onclick = async function(){
+    let taskId = ++cnt;
+    let roomids = [...input.value.matchAll(/\d+/g)].map(v=>Number(v[0]));
+    console.log(roomids);
+    let infos = await Promise.all(roomids.map(GetParsedInfo));
+    infos.forEach(info => {
+        console.log(info);
+    });
+    ShowList(infos);
+    let task = setInterval(async function(){
+        if (taskId !== cnt){
+            clearInterval(task);
+        }
+        let newInfos = await Promise.all(roomids.map(GetParsedInfo));
+        // eslint-disable-next-line guard-for-in
+        for(let i in newInfos){
+            let newInfo = newInfos[i];
+            let prev = infos[i].status;
+            let cur = newInfo.status;
+            if (cur !== prev){
+                infos[i] = newInfo;
+                if (cur){
+                    SendNotification(newInfo.name + '开播了', newInfo.title, newInfo.url);
+                }
+            }
+        }
+    }, 30 * 1000);
+};
+
+window.onkeydown = (ev)=>{
+    if(ev.key === 'F12'){
+        ipcRenderer.send('asynchronous-message', JSON.stringify({
+            action: 'f12'
+        }));
+    }
+};
